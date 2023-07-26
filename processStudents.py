badd1123da0e85e8d39e9bc73fb14a346b83da3e
Kendall Mauer <kendall_mauer1@baylor.edu>
2023-07-25 22:55:05 -0500

    initial commit

--- processStudents.py
+++ processStudents.py
@@ -24,51 +24,58 @@ import csv
 
 
 # create a file object to open the file in read mode
-
+with open('students.csv', 'r') as infile:
 
 
 # create a csv object from the file object
-
+    read = csv.reader(infile)
 
 #skip the header row
-
+    header = next(read)
 
 #create an outfile object for the pocessed record
-
+    with open ('processedStudents.csv', 'a') as outfile:
+        write = csv.writer(outfile)
 
 
 #create a new dictionary named 'student_dict'
-
+        student_dict = {}
 
 
 #use a loop to iterate through each row of the file
+        for row in read:
+            stud_id = row[0]
+            gpa = float(row[8])
 
     #check if the GPA is below 3.0. If so, write the record to the outfile
-    
+
+            if gpa < 3.0:
+                write.writerow(row)
         
 
 
 
     # append the record to the dictionary with the student id as the Key
     # and the value as the GPA
-    
+    student_dict['stud_id'] = 'gpa'
 
 
 
 
 
 #print the entire dictionary
-
+for stud_id, gpa in student_dict.items():
+    print(f"{stud_id},{gpa}")
 
 #Print the student id 
-
-
+if stud_id in  
+    print(student_dict['567890123'])
+'''
 #print out the corresponding GPA from the dictionary
+for student_id, gpa in student_dict.items():
+    print(f"The type of pet is: {i} and the name of the pet is: {j}")
 
-
-
-#close the outfile
-
+'''
 
 
 
